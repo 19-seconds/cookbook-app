bb3d2a16c351fc115367803fa777ac83b06ba353
Mark Richardson <markeban@yahoo.com>
2017-01-18 11:37:00 +0200

    initial commit

---
+++ db/migrate/20170118090114_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :ingredients
+      t.string :directions
+      t.string :image
+      t.integer :prep_time
+
+      t.timestamps
+    end
+  end
+end
