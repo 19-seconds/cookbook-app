2443a5b3db11daff64ae6a238c2ba5d0e24980db
Mark Richardson <markeban@yahoo.com>
2017-01-23 14:49:48 +0200

    added new and create actions, and created links to show page from index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,16 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find_by(id: params[:id])
   end
+
+  def new
+  end
+
+  def create
+    title = params[:title]
+    ingredients = params[:ingredients]
+    prep_time = params[:prep_time]
+    image = params[:image]
+    recipe = Recipe.new({title: title, ingredients: ingredients, prep_time: prep_time, image: image  })
+    recipe.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/recipes", to: 'recipes#index'
+  get "/recipes/new", to: 'recipes#new'
   get "/recipes/:id", to: 'recipes#show'
+  post "/recipes", to: 'recipes#create'
 end
